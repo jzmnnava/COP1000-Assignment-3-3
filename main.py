40b6fa55b1647a3bebbd3001e8d4339b7b5a5c81
jzmnnava <146144314+jzmnnava@users.noreply.github.com>
2024-09-13 22:06:19 +0000

    Added Description

--- main.py
+++ main.py
@@ -8,27 +8,40 @@ MIN_MONTH = 1
 MAX_MONTH = 12
 MIN_DAY = 1
 MAX_DAY = 31
-
+LEAP_YEAR = MIN_YEAR % 4 == 0 and (MIN_YEAR % 100 == 0 or MIN_YEAR % 400 == 0) 
 year = None
-month = None
+month = None 
 day = None
 
 # Get the year, then the month, then the day
 # housekeeping()
 
-# Check to be sure date is valid
+month = int(input("Enter a month: "))
+day = int(input("Enter a day: "))
+year = int(input("Enter a year: "))
 
-if int(year) <= MIN_YEAR: # invalid year
+# Check to be sure date is valid
+validDate = True
+
+if int(year) <= MIN_YEAR:
     validDate = False
-elif int(month) < MIN_MONTH or int(month) > MAX_MONTH: # invalid month
+elif int(month) < MIN_MONTH:
     validDate = False
-elif int(day) < MIN_DAY or int(day) > MAX_DAY: # invalid day
+elif int(month) > MAX_MONTH:
     validDate = False
+elif int(day) < MIN_DAY:
+    validDate = False
+elif int(day) > MAX_DAY:
+    validDate = False
+elif month == 2 and day > 28 and year % 4 == 0 and year % 100 == 0 or year % 400 == 0:
+    validDate = False
+elif month == 4 or month == 6 or month == 9 or month == 11 and day > 30:
+    validDate = False    
+# invalid day for April, June, September, November, February for a non leap year
 
 # Test to see if date is valid and output date and whether it is valid or not
-
 # endOfJob()
-if validDate == True:
+if validDate == True: print(str(month) + "/" + str(day) + "/" + str(year) + " is a valid date") 
     # Output statement
-else:
+else: print(str(month) + "/" + str(day) + "/" + str(year) + " is an invalid date") 
     # Output statement
